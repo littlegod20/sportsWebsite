f3e3f4713cde82a26184c146786efba5c29bbc8c
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-11-19 19:14:01 +0000

    added lazy loading to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import "./App.css";
-import AllSports from "./pages/AllSports";
-import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MyBets from "./pages/MyBets";
-import LiveNow from "./pages/LiveNow";
-import Account from "./pages/Account";
 import Layout from "./Layout";
+import { lazy } from "react";
+import Home from "./pages/Home";
+
+const Account = lazy(() => import("./pages/Account"));
+const LiveNow = lazy(() => import("./pages/LiveNow"));
+const MyBets = lazy(() => import("./pages/MyBets"));
+const AllSports = lazy(() => import("./pages/AllSports"));
 
 function App() {
   return (
     <main>
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout/>}>
-
-          <Route path="/" element={<Home />} />
-          <Route path="/allSports" element={<AllSports />} />
-          <Route path="/myBets" element={<MyBets />} />
-          <Route path="/liveNow" element={<LiveNow />} />
-          <Route path="/account" element={<Account />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/allSports" element={<AllSports />} />
+            <Route path="/myBets" element={<MyBets />} />
+            <Route path="/liveNow" element={<LiveNow />} />
+            <Route path="/account" element={<Account />} />
           </Route>
         </Routes>
       </BrowserRouter>
